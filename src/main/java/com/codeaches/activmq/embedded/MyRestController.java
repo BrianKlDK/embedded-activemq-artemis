ed8e799a1271fac4685f4f67d902b7d5a3188ec4
Brian Skovgren Klausen <brian@lbrt.dk>
2024-10-16 11:26:54 +0200

    Merge ready

--- src/main/java/com/codeaches/activmq/embedded/MyRestController.java
+++ src/main/java/com/codeaches/activmq/embedded/MyRestController.java
@@ -1,5 +1,7 @@
 package com.codeaches.activmq.embedded;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,11 +12,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class MyRestController {
 
+    Logger log = LoggerFactory.getLogger(MyRestController.class);
+
     @Autowired
     JmsProducer jmsProducer;
 
     @PostMapping("/send")
     public void sendDataToJms(@RequestParam String message) {
-	jmsProducer.send(message);
+        log.info("Received request on REST Controller with payload='{}'", message);
+    	jmsProducer.send(message);
+        log.info("Request put on queue - responding to REST client");
     }
 }
