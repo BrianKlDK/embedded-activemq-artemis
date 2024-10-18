dba4dfdfa49dc86c7f75a5a3b91c55cd946fc4f6
Brian Skovgren Klausen <brian@lbrt.dk>
2024-10-18 14:45:34 +0200

    Artemis with JDBC persistence

--- src/main/java/com/codeaches/activmq/embedded/ArtemisConfig.java
+++ src/main/java/com/codeaches/activmq/embedded/ArtemisConfig.java
@@ -1,5 +1,7 @@
 package com.codeaches.activmq.embedded;
 
+import org.apache.activemq.artemis.core.config.StoreConfiguration;
+import org.apache.activemq.artemis.core.config.storage.DatabaseStorageConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.autoconfigure.jms.artemis.ArtemisConfigurationCustomizer;
@@ -12,6 +14,15 @@ public class ArtemisConfig implements ArtemisConfigurationCustomizer {
     @Override
     public void customize(org.apache.activemq.artemis.core.config.Configuration configuration) {
         //configuration.addAcceptorConfiguration("remote", "tcp://0.0.0.0:61616");
-        log.info("Artemis config: {}", configuration.isJDBC());
+        log.info("Artemis config: isJDBC: {}", configuration.isJDBC());
+        log.info("Artemis config: isPersistenceEnabled: {}", configuration.isPersistenceEnabled());
+        log.info("Artemis config: toString {}", configuration.toString());
+        DatabaseStorageConfiguration storeConfiguration = new DatabaseStorageConfiguration();
+        storeConfiguration.setJdbcConnectionUrl("jdbc:mysql://172.17.0.2:3306/artemis");
+        storeConfiguration.setJdbcDriverClassName("com.mysql.cj.jdbc.Driver");
+        storeConfiguration.setJdbcUser("artemis");
+        storeConfiguration.setJdbcPassword("artemis");
+        configuration.setStoreConfiguration(storeConfiguration);
+        //log.info("Artemis config: toString {}", configuration.toString());
     }
 }
